c5b5ed7bf56203d7cf9c3c4aa60834a460d1fcc8
Paul Smith <paulcsmith0218@gmail.com>
2018-03-30 19:08:18 -0400

    Add validate_uniqueness_of

--- src/forms/sign_up_form.cr
+++ src/forms/sign_up_form.cr
@@ -4,10 +4,35 @@ class SignUpForm < User::BaseForm
   allow_virtual password_confirmation : String
 
   def prepare
-    # TODO: add validate_uniqueness email
+    validate_uniqueness_of(email, query: UserQuery.new.email, message: "is not available")
     validate_required password, password_confirmation
     validate_confirmation_of password, with: password_confirmation
     validate_size_of password, min: 6
     Authentic::SignUpFormHelpers.save_encrypted password, to: encrypted_password
+  end
+
+  private def validate_uniqueness_of(
+    field,
+    query : LuckyRecord::Criteria? = nil,
+    message = "is already taken"
+  )
+    field.value.try do |value|
+      prepared_query = prepare_query(query, field.name, value)
+      if prepared_query.first?
+        field.add_error message
+      end
+    end
+  end
+
+  private def prepare_query(
+    query : LuckyRecord::Criteria?,
+    column_name,
+    value
+  ) : T::BaseQuery
+    if query
+      query.is(value)
+    else
+      T::BaseQuery.new.where(column_name, value)
+    end
   end
 end
