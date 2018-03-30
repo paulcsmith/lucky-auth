ccb3e86c290e9677100e8be8f1be08c39cf42816
Paul Smith <paulcsmith0218@gmail.com>
2018-03-30 19:30:00 -0400

    Properly humanize label text

--- src/components/shared/field_errors.cr
+++ src/components/shared/field_errors.cr
@@ -1,9 +1,10 @@
 module Shared::FieldErrors
+  # Customize the markup and styles to match your application
   def errors_for(field : LuckyRecord::AllowedField)
-    # Customize the markup and styles to match your application
     unless field.valid?
       div class: "error" do
-        text "#{field.name.to_s.capitalize} #{field.errors.join(", ")}"
+        label_text = LuckyInflector::Inflector.humanize(field.name.to_s)
+        text "#{label_text} #{field.errors.join(", ")}"
       end
     end
   end
