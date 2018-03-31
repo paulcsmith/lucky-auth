2d1802e91c5b194738498c4188db8c1859cbd179
Paul Smith <paulcsmith0218@gmail.com>
2018-03-30 20:01:18 -0400

    Clarify instructions for validating sign in

--- src/forms/sign_in_form.cr
+++ src/forms/sign_in_form.cr
@@ -6,17 +6,21 @@ class SignInForm < LuckyRecord::VirtualForm
   allow_virtual email : String
   allow_virtual password : String
 
+  # This method is called to allow you to determine if a user can sign in.
+  # By default it validates that the user exists and the password is correct.
+  #
+  # If desired, you can add additional checks in this method, e.g.
+  #
+  #    if user.locked?
+  #      email.add_error "is locked out"
+  #    end
   private def validate_allowed_to_sign_in(user : User)
     Authentic.on_incorrect_password(user, password) do
       password.add_error "is incorrect"
     end
-    # You can add additional checks in this method
-    #
-    #    if user.locked?
-    #      email.add_error "is locked out"
-    #    end
   end
 
+  # Called when the user is not found (Nil)
   private def validate_allowed_to_sign_in(user_not_found : Nil)
     email.add_error "is not in our system"
   end
