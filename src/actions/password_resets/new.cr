76257e059ffdc363e0209b9853a3402e9bb5bf69
Paul Smith <paulcsmith0218@gmail.com>
2018-04-06 12:54:07 -0400

    Add docs about password reset safety

--- src/actions/password_resets/new.cr
+++ src/actions/password_resets/new.cr
@@ -4,6 +4,12 @@ class PasswordResets::New < BrowserAction
   param token : String
 
   get "/password_resets/:user_id" do
+    redirect_to_edit_form_without_token_param
+  end
+
+  # This is to prevent password reset tokens from being scraped in the HTTP Referer header
+  # See more info here: https://github.com/thoughtbot/clearance/pull/707
+  private def redirect_to_edit_form_without_token_param
     make_token_available_to_future_actions
     redirect to: PasswordResets::Edit.with(user_id)
   end
